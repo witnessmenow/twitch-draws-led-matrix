cec5fcbfa6b3c7d2c2a7c499bdd7d76d1bc77ff7
witnessmenow <ecenuig@gmail.com>
2018-07-17 23:38:43 +0100

    Fixes from the stream

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var colourConversion = {
   green: '0x07E0',
   blue: '0x001F',
   purple: '0xF81F',
-  lightBlue: '0x07FF',
+  lightblue: '0x07FF',
   yellow: '0xFFE0',
   white: '0xFFFF',
   black: '0x0000',
@@ -29,7 +29,7 @@ client.on('connect', function(connection) {
   });
 });
 
-client.connect('ws://192.168.1.18:81');
+client.connect('ws://192.168.1.24:81');
 
 const Bot = new TwitchBot({
  username: 'ledmatrix',
@@ -41,28 +41,128 @@ Bot.on('join', () => {
 
   console.log('joined Channel');
 
+  //let message = chatter.message.replace
  Bot.on('message', chatter => {
-   if(chatter.message.indexOf('!draw') === 0) {
+  if(chatter.message.indexOf('!help') === 0)
+  {
+    Bot.say('help!');
+    Bot.say('!draw x y col');
+    Bot.say('!rect x y w h col');
+    Bot.say('!line x1 y1 x2 y2 col');
+    Bot.say('!circle x y r col');
+    Bot.say('Colours: ' + colourOptions.join(','));
+  }
+  else if(chatter.message.indexOf('!draw') === 0)
+  {
       var message = chatter.message.replace('!draw ', '');
       var splitMessage = message.split(' ');
       if(splitMessage.length !== 3){
-        Bot.say('Wrong number of params: e.g. !draw 0 0 red');
+        Bot.say('Wrong number of params: e.g. !draw x y col');
       } else {
         var x = splitMessage[0];
         var y = splitMessage[1];
         var colour = splitMessage[2].toLowerCase();
         var convertedColour = colourConversion[colour];
         
-        if (convertedColour){
-          var messageCommand = x + ',' + y + ',' + convertedColour;
+        if (!isNaN(x) && !isNaN(y) && convertedColour){
+          var messageCommand = '0:'+ x + ',' + y + ',' + convertedColour;
           console.log(messageCommand);
           socketConnection.sendUTF(messageCommand);
         } else {
-          var colourOptions = object.keys(colourConversion);
+          var colourOptions = Object.keys(colourConversion);
           Bot.say('Unsupported Colour, use: ' + colourOptions.join(','));
         }
       }
    }
+   else if(chatter.message.indexOf('!rect') === 0)
+   {
+    var message = chatter.message.replace('!rect ', '');
+    var splitMessage = message.split(' ');
+    if(splitMessage.length !== 5)
+    {
+      Bot.say('Wrong number of params: e.g. !rect x y w h col');
+    }
+    else 
+    {
+      var x = splitMessage[0];
+      var y = splitMessage[1];
+      var w = splitMessage[2];
+      var h = splitMessage[3];
+      var colour = splitMessage[4].toLowerCase();
+      var convertedColour = colourConversion[colour];
+      
+      if (!isNaN(x) && !isNaN(y) && !isNaN(w) && !isNaN(h) &&convertedColour)
+      {
+        var messageCommand = "1:" + x + ',' + y + ',' + w + ',' + h + ',' + convertedColour;
+        console.log(messageCommand);
+        socketConnection.sendUTF(messageCommand);
+      }
+      else
+      {
+        var colourOptions = Object.keys(colourConversion);
+        Bot.say('Unsupported Colour, use: ' + colourOptions.join(','));
+      }
+    }
+  }
+  else if(chatter.message.indexOf('!line') === 0)
+   {
+    var message = chatter.message.replace('!line ', '');
+    var splitMessage = message.split(' ');
+    if(splitMessage.length !== 5)
+    {
+      Bot.say('Wrong number of params: e.g. !line x1 y1 x2 y2 col');
+    }
+    else 
+    {
+      var x = splitMessage[0];
+      var y = splitMessage[1];
+      var w = splitMessage[2];
+      var h = splitMessage[3];
+      var colour = splitMessage[4].toLowerCase();
+      var convertedColour = colourConversion[colour];
+      
+      if (!isNaN(x) && !isNaN(y) && !isNaN(w) && !isNaN(h) &&convertedColour)
+      {
+        var messageCommand = "2:" + x + ',' + y + ',' + w + ',' + h + ',' + convertedColour;
+        console.log(messageCommand);
+        socketConnection.sendUTF(messageCommand);
+      }
+      else
+      {
+        var colourOptions = Object.keys(colourConversion);
+        Bot.say('Unsupported Colour, use: ' + colourOptions.join(','));
+      }
+    }
+  }
+  else if(chatter.message.indexOf('!circle') === 0)
+   {
+    var message = chatter.message.replace('!circle ', '');
+    var splitMessage = message.split(' ');
+    if(splitMessage.length !== 4)
+    {
+      Bot.say('Wrong number of params: e.g. !circle x y r col');
+    }
+    else 
+    {
+      var x = splitMessage[0];
+      var y = splitMessage[1];
+      var r = splitMessage[2];
+      var colour = splitMessage[3].toLowerCase();
+      var convertedColour = colourConversion[colour];
+      
+      if (!isNaN(x) && !isNaN(y) && !isNaN(r) && convertedColour)
+      {
+        var messageCommand = "3:" + x + ',' + y + ',' + r + ',' + convertedColour;
+        console.log(messageCommand);
+        socketConnection.sendUTF(messageCommand);
+      }
+      else
+      {
+        var colourOptions = Object.keys(colourConversion);
+        Bot.say('Unsupported Colour, use: ' + colourOptions.join(','));
+      }
+    }
+  }
  })
 })
 
